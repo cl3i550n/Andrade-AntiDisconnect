5cbc645adcee29a06467bd8ca729c553d438fe49
clandrade <cleisson@outlook.com.br>
2024-01-30 13:02:07 -0300

    added readme and ajusts

--- nui/jquery.js
+++ nui/jquery.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
-    window.addEventListener("offline", function () { // Quando a internet do jogador cai o evento é acionado.
-        $("#displayNui").css("display", "flex") // Exibe a nui na tela do jogador.
-        $.post(`https://${GetParentResourceName()}/lock`) // Manda a informação pro client para ele 'trabalhar'
+    window.addEventListener("offline", function () {
+        $("#displayNui").css("display", "flex")
+        $.post(`https://${GetParentResourceName()}/lock`)
     });
 
-    window.addEventListener("online", function () { // Quando a internet do jogador volta o evento é acionado
-        setTimeout(function () { // Timeout por conta do delay do FiveM
-            $("#displayNui").css("display", "none") // Remove a nui da tela do jogador
-            $.post(`https://${GetParentResourceName()}/unlock`) // Manda a informação pro client para ele 'trabalhar'
+    window.addEventListener("online", function () {
+        setTimeout(function () {
+            $("#displayNui").css("display", "none")
+            $.post(`https://${GetParentResourceName()}/unlock`)
         }, 5000)
     });
 });
